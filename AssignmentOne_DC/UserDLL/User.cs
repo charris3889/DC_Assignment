299f0c45510143cef13414577cf4cf5db8371ead
totid <cvillena0924@gmail.com>
2023-08-30 14:35:49 +0800

    Added basic interpretation of User would look like, still early stages. Bare with me

--- AssignmentOne_DC/UserDLL/User.cs
+++ AssignmentOne_DC/UserDLL/User.cs
@@ -9,6 +9,28 @@ namespace UserDLL
     public class User
     {
         private string name;
+        private List<string> currentChat;   
+        private Dictionary<string, List<string>> chatRooms; 
+
+
+        public User(string userName) 
+        {
+            name = userName;
+            currentChat = null;
+            chatRooms = new Dictionary<string, List<string>>();
+        }
+
+
+        public void setCurrentChat(List<string> currentChat)    //assign a current chat whenever a groupchat or person is clicked
+        {
+            this.currentChat = currentChat;
+        }
+
+        public void addChatRoom(string chatName, List<String> roomName)     //whenever a chatroom is created
+        {
+            chatRooms.Add(chatName, roomName);
+        }
+
 
     }
 }
