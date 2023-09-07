76aa3e956e0e9b7d4d93b3d91003f9a62f716d42
charris3889 <cameron.harris3889@gmail.com>
2023-09-07 16:15:39 +0800

    Started on login and created serv int
    Started on login page,
    Created server interface to obtain methods for client side

--- AssignmentOne_DC/ChatUserOne/MainWindow.xaml.cs
+++ AssignmentOne_DC/ChatUserOne/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel;
 
 namespace ChatUserOne
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
     public partial class MainWindow : Window
     {
+        private ServerInterface foob;
         public MainWindow()
         {
             InitializeComponent();
+
+            ChannelFactory<ServerInterface> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8100/Server";
+            foobFactory = new ChannelFactory<ServerInterface>(tcp, URL);
+            foob = foobFactory.CreateChannel();
+        }
+
+        public void LoginButton_Click()
+        {
+            //Check whether username exists on server
+            //Create user on server and change layout
+
+            string username = UsernameBox.Text;
+
+            if (!usernameExists(username))
+            {
+                createUser(username);
+                //Change layout to chat room
+            }
+            else
+            {
+                ErrorBlock.Text = "Username already exists please try a different name";
+            }
         }
     }
 }
---
+++ AssignmentOne_DC/Server/ServerInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace ServerDLL
+{
+    public interface ServerInterface
+    {
+        void createUser(string username);
+        void createChatrooom(string chatname);
+        Boolean usernameExists(string username);
+        void joinChatroom(string username, string chatname);
+        void sendMessage(string message, string chatname);
+
+    }
+}
